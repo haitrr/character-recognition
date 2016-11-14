bb7908cc383b42dfd010596eb752f31ac2409fed
Hai Tran <vroyibg@gmail.com>
2016-11-14 22:10:47 +0700

    Added use interface

--- CharacterRecognition.py
+++ CharacterRecognition.py
@@ -2,152 +2,163 @@ import numpy as np
 from PIL import Image
 import tkinter
 
-BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-size = 16, 16
 
 
-# sigmoid function
-def save_weights(syn):
-    f = open("weights.txt", 'w')
-    f.write("SYN0\n")
-    for i in syn[0]:
-        for j in i:
-            f.write(str(j) + "\n")
-    f.write("SYN1\n")
-    for i in syn[1]:
-        for j in i:
-            f.write(str(j) + "\n")
-    f.close()
+class CharacterReconition(object):
+    def __init__(self):
+        self.output_layer = None
+        self.input_layer = None
+        self.syn = None
+        self.input_size = 16, 16
+        self.l1_size=90
+        self.output_size=62
+        self.sample_size=20
+        self.char = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        return
+        # sigmoid function
 
+    #load input and output layer
+    def load_input_output(self):
+        self.input_layer = self.get_input_from_samples()
+        self.output_layer = self.get_output_layer()
 
-def load_weight():
-    f = open("weights.txt", 'r')
-    f.readline()
-    t = []
-    t2 = []
-    t3 = []
-    for i in range(0,256):
+    #storage weights to file
+    def save_weights(self):
+        f = open("weights.txt", 'w')
+        f.write("SYN0\n")
+        for i in self.syn[0]:
+            for j in i:
+                f.write(str(j) + "\n")
+        f.write("SYN1\n")
+        for i in self.syn[1]:
+            for j in i:
+                f.write(str(j) + "\n")
+        f.close()
+
+    def load_weight(self):
+        f = open("weights.txt", 'r')
+        f.readline()
         t = []
-        for i in range(0,90):
-            t.append(float(f.readline()))
-        t2.append(t)
-    f.readline()
-    for i in range(0, 90):
-        t = []
-        for i in range(0, 62):
-            t.append(float(f.readline()))
-        t3.append(t)
-    return t2,t3
-def sigmoid(x, deriv=False):
-    if deriv:
-        return x * (1 - x)
-    return 1 / (1 + np.exp(-x))
+        t2 = []
+        t3 = []
+        for i in range(0, self.input_size[0]*self.input_size[1]):
+            t = []
+            for i in range(0, self.l1_size):
+                t.append(float(f.readline()))
+            t2.append(t)
+        f.readline()
+        for i in range(0, self.l1_size):
+            t = []
+            for i in range(0, self.output_size):
+                t.append(float(f.readline()))
+            t3.append(t)
+        self.syn = t2, t3
 
+    def sigmoid(self, x, deriv=False):
+        if deriv:
+            return x * (1 - x)
+        return 1 / (1 + np.exp(-x))
 
-def get_input_from_samples():
-    input_layer = []
-    for i in range(1, 63):
-        print("Loading sample " + str(i))
-        for j in range(1, 21):
-            input_layer.append((get_pixels(
-                "Samples" + "\Sample" + str(i).zfill(3) + "\img" + str(i).zfill(3) + "-" + str(j).zfill(5) + ".png")))
-    return np.array(input_layer)
+    def get_input_from_samples(self):
+        input_layer = []
+        for i in range(1, self.output_size+1):
+            print("Loading sample " + str(i))
+            for j in range(1, self.sample_size+1):
+                input_layer.append((self.get_pixels(
+                    "Samples" + "\Sample" + str(i).zfill(3) + "\img" + str(i).zfill(3) + "-" + str(j).zfill(
+                        5) + ".png")))
+        return np.array(input_layer)
 
+    def get_pixels(self, image):
+        im = Image.open(image)
+        im.thumbnail(self.input_size)
+        rgb_im = im.convert('RGB')
+        input_vector = []
+        for i in range(0, self.input_size[1]):
+            for j in range(0, self.input_size[0]):
+                pixel = rgb_im.getpixel((i, j))
+                if pixel != WHITE:
+                    input_vector.append(1)
+                else:
+                    input_vector.append(0)
+        return np.array(input_vector)
 
-def get_pixels(image):
-    im = Image.open(image)
-    im.thumbnail(size)
-    rgb_im = im.convert('RGB')
-    input_vector = []
-    for i in range(0, 16):
-        for j in range(0, 16):
-            pixel = rgb_im.getpixel((i, j))
-            if pixel != WHITE:
-                input_vector.append(1)
-            else:
-                input_vector.append(0)
-    return np.array(input_vector)
+    def get_output_layer(self):
+        output_layer = []
+        for i in range(1, self.output_size+1):
+            output = []
+            for k in range(1, self.output_size+1):
+                if k == i:
+                    output.append(1)
+                else:
+                    output.append(0)
+            for j in range(1, self.sample_size+1):
+                output_layer.append(output)
+        return np.array(output_layer)
 
+    def train(self, input, output, times,resume= False):
+        #continue training?
+        if resume :
+            syn = self.load_weight()
+            syn0= syn[0]
+            syn1 = syn[1]
+        else:
+            # seed random numbers
+            np.random.seed(1)
+            # initialize weights randomly with mean 0
+            syn0 = 2 * np.random.random((self.input_size[1]*self.input_size[2], self.l1_size)) - 1
+            syn1 = 2 * np.random.random((self.l1_size, self.l2)) - 1
 
-def get_output_layer():
-    output_layer = []
-    output = []
-    for i in range(1, 63):
-        output = []
-        for k in range(1, 63):
-            if k == i:
-                output.append(1)
-            else:
-                output.append(0)
-        for j in range(1, 21):
-            output_layer.append(output)
-    return np.array(output_layer)
+        for j in range(times):
+            # forward propagation
+            l0 = self.input_layer
+            l1 = self.sigmoid(np.dot(l0, syn0))
+            l2 = self.sigmoid(np.dot(l1, syn1))
 
+            # how much did we miss?
+            l2_error = self.output_layer - l2
+            if (j % 100) == 0:
+                print("Error:" + str(np.mean(np.abs(l2_error))))
+            l2_delta = l2_error  # * sigmoid(l2, deriv=True)
 
-output_layer = get_output_layer()
-input_layer = get_input_from_samples()
+            l1_error = l2_delta.dot(syn1.T)
+            # multiply how much we missed by the
+            # slope of the sigmoid at the values in l1
+            l1_delta = l1_error  # * sigmoid(l1, True)
 
+            # update weights
+            syn1 += l1.T.dot(l2_delta)
+            syn0 += l0.T.dot(l1_delta)
+        return syn0, syn1
 
-def train(input, output, times):
-    # seed random numbers
-    np.random.seed(1)
-    # initialize weights randomly with mean 0
-    syn0 = 2 * np.random.random((256, 90)) - 1
-    syn1 = 2 * np.random.random((90, 62)) - 1
+    def sample(self, i, j):
+        return "Samples" + "\Sample" + str(i).zfill(3) + "\img" + str(i).zfill(3) + "-" + str(j).zfill(5) + ".png"
 
-    for j in range(times):
-        # forward propagation
-        l0 = input_layer
-        l1 = sigmoid(np.dot(l0, syn0))
-        l2 = sigmoid(np.dot(l1, syn1))
+    def reconize(self, image):
+        l0 = self.get_pixels(image)
+        l1 = self.sigmoid(np.dot(l0, self.syn[0]))
+        l2 = self.sigmoid(np.dot(l1, self.syn[1]))
+        for i in range(0, self.output_size):
+            print(str(i + 1) + "    " + str(l2[i]))
+        return self.char[l2.argmax()]
 
-        # how much did we miss?
-        l2_error = output_layer - l2
-        if (j % 100) == 0:
-            print("Error:" + str(np.mean(np.abs(l2_error))))
-        l2_delta = l2_error  # * sigmoid(l2, deriv=True)
-
-        l1_error = l2_delta.dot(syn1.T)
-        # multiply how much we missed by the
-        # slope of the sigmoid at the values in l1
-        l1_delta = l1_error  # * sigmoid(l1, True)
-
-        # update weights
-        syn1 += l1.T.dot(l2_delta)
-        syn0 += l0.T.dot(l1_delta)
-    return syn0, syn1
-
-
-def sample(i, j):
-    return "Samples" + "\Sample" + str(i).zfill(3) + "\img" + str(i).zfill(3) + "-" + str(j).zfill(5) + ".png"
-
-
-def reconize(image, syn0, syn1):
-    l0 = get_pixels(image)
-    l1 = sigmoid(np.dot(l0, syn0))
-    l2 = sigmoid(np.dot(l1, syn1))
-    for i in range(0, 62):
-        print(str(i + 1) + "    " + str(l2[i]))
-    print(l2.argmax())
-
-def accurate(syn0, syn1):
-    count = 0
-    l0 = input_layer
-    l1 = sigmoid(np.dot(l0, syn0))
-    l2 = sigmoid(np.dot(l1, syn1))
-    for t in range(0, 62):
-        for l in range(0,20):
-            maxs = output_layer[t][l].argmax()
-            maxo = l2[t][l].argmax()
-            if maxs == maxo:
-                count = count + 1
-    print("Accuracy rate : " + str(count) + " " + str(count * 100 / 1240))
-
+    def accurate(self, syn0, syn1):
+        count = 0
+        l0 = self.input_layer
+        l1 = self.sigmoid(np.dot(l0, syn0))
+        l2 = self.sigmoid(np.dot(l1, syn1))
+        for t in range(0, self.output_size):
+            for l in range(0, self.sample_size):
+                maxs = self.output_layer[t][l].argmax()
+                maxo = l2[t][l].argmax()
+                if maxs == maxo:
+                    count = count + 1
+        print("Accuracy rate : " + str(count) + " " + str(count * 100 / 1240))
 
 # print("Output After Training:")
 # print(l2)
-#syn = train(input_layer, output_layer, 20000)
-syn = load_weight()
-#save_weights(syn)
-accurate(syn[0], syn[1])
+# syn = train(input_layer, output_layer, 20000)
+# syn = load_weight()
+# save_weights(syn)
+# accurate(syn[0], syn[1])
