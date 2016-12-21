39833849f7f12c36b115d66332ebc88cdc71e19a
Hai Tran <vroyibg@gmail.com>
2016-12-21 09:25:28 +0700

    Release

--- App.py
+++ App.py
@@ -2,55 +2,94 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from PIL import ImageTk, Image
-from CharacterRecognition import CharacterReconition
-
+import Data
+import NeuralNetwork
+import numpy as np
 
 class App(object):
-    def __init__(self):
+    def __init__(self,neural_net):
+        # Setup window
         self.root = Tk()
         self.style = ttk.Style()
         self.style.theme_use('winnative')
         self.root.title("Character Reconition")
+        self.root.grid()
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
 
-        # frame
-        frm = Frame(self.root)
-        frm.pack(expand=True, fill='both')
-        self.file_name = ""
-
-        # image
-        self.image = None
-        self.image_panel = Label(self.root, image=self.image)
-        self.image_panel.pack(side="bottom", fill="both", expand="yes")
+        # Init variables
+        self.images = []
+        self.image_panels = []
+        self.result_lables=[]
+        self.files = []
+        self.entry_per_row = 7
 
         # open image button
         self.open_image_button = Button(self.root, text="Choose image", command=self.open_image_button_click)
-        self.open_image_button.pack(side="top", fill="both")
+        self.open_image_button.grid(row=0,column=0,columnspan = self.entry_per_row,sticky=N+S+E+W)
 
         # reconize button
         self.reconize_button = Button(self.root, text="Reconize", command=self.reconize_button_click)
-        self.reconize_button.pack(side="left", fill="both")
-
-        # Result lable
-        self.resuilt_lable = Label(self.root, text="")
-        self.resuilt_lable.pack(side="right", fill="both")
+        self.reconize_button.grid(row=1,column=0,columnspan = self.entry_per_row,sticky=N+S+E+W)
 
         # Character recognition
-        self.character_recognition = CharacterReconition()
-        self.character_recognition.load_weight()
+        self.character_recognition = neural_net
 
+
+        # Set default layout
+        self.files.append("default.png")
+        self.set_layout()
     def open_image_button_click(self):
-        self.file_name = filedialog.askopenfilename(initialdir= "Samples", #"D:\Download\EnglishFnt\EnglishFnt\English\Fnt",
+        files = filedialog.askopenfilenames(initialdir= "Samples",
                                                     title="Choose image",
                                                     filetypes=(("png files", "*.png"), ("all files", "*.*")))
-        if self.file_name is not "":
-            self.image = ImageTk.PhotoImage(Image.open(self.file_name))
-            self.image_panel.config(image=self.image)
-            self.image_panel.pack(side="bottom", fill="both", expand="yes")
 
+        self.files = list(files)
+        self.set_layout()
+    def set_layout(self):
+
+        # Clean previous entries
+        for panel in self.image_panels:
+            panel.destroy()
+        for lable in self.result_lables:
+            lable.destroy()
+        self.image_panels.clear()
+        self.result_lables.clear()
+
+        # Show new entries
+        for file in self.files:
+            # Show images
+            image = Image.open(file)
+            image = image.resize((150, 150), Image.ANTIALIAS)
+            image = ImageTk.PhotoImage(image)
+            image_panel = Label(self.root, image=image)
+            image_panel.image = image
+            r = int((self.files.index(file)) / self.entry_per_row) * 2 + 2
+            c = (self.files.index(file)) % self.entry_per_row
+            image_panel.grid(row=r, column=c)
+
+            # Store panels
+            self.image_panels.append(image_panel)
+
+            # Create result lables
+            result_lable = Label(self.root, width=10, text="?", font=("Arial", 16))
+            result_lable.grid(row=r + 1, column=c)
+
+            # Create result labels
+            self.result_lables.append(result_lable)
     def reconize_button_click(self):
-        rs = self.character_recognition.reconize(self.file_name)
-        self.resuilt_lable.config(text=rs)
+
+        # Get input from files
+        ip= [Data.get_pixels(file) for file in self.files]
+
+        # Recognize the images using neural network
+        rs = [Data.char[np.argmax(self.character_recognition.feed_forward(i))] for i in ip]
+
+        # Display the results
+        for lable,result in zip(self.result_lables,rs):
+            lable.config(text=str(result))
 
 
-app = App()
+file = "network16.nn"
+app = App(NeuralNetwork.load(file))
 app.root.mainloop()
---
+++ Data.py
@@ -0,0 +1,77 @@
+from PIL import Image
+import numpy as np
+
+from os import listdir
+
+en_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+           'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+           'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+vn_char = ['Ă', 'Â',
+           'Đ', 'Ê',
+           'Ô', 'Ơ', 'Ư', 'ă', 'â', 'đ', 'ê', 'ô', 'ơ', 'ư']
+
+char = en_char + vn_char
+input_size = (16, 16)
+
+output_size = 66
+
+samples = "Samples/Sample"
+
+test = "Tests/Sample"
+
+
+# generate the output layer
+def load_output(sample_size):
+    output_layer = get_output_layer(sample_size)
+
+
+# get input from samples
+def get_input_from_samples(data):
+    input_layer = []
+    output_layer = []
+    for i in range(1, output_size + 1):
+        print("Loading sample " + str(i))
+        for j in listdir(data + str(i).zfill(3)):
+            input_layer.append(get_pixels(data + str(i).zfill(3) + "/" + j))
+            temp = np.zeros((output_size, 1))
+            temp[i - 1] = 1
+            output_layer.append(temp)
+    return list(zip(input_layer, output_layer))
+
+
+def scale(val, src, dst):
+    """
+    Scale the given value from the scale of src to the scale of dst.
+    """
+    return ((val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
+
+
+# get pixel from image
+def get_pixels(image):
+    im = Image.open(image)
+    input_vector = []
+    pixels = list(im.getdata())
+    for i in pixels:
+        input_vector.append(1 - scale(sum(i), (0, 765), (0, 1)))
+    return np.reshape(input_vector, (input_size[0] * input_size[1], 1))
+
+
+# generate input layer
+def get_output_layer(sample_size):
+    output_layer = []
+    for i in range(0, output_size):
+        for j in range(0, sample_size):
+            temp = np.zeros((output_size, 1))
+            temp[i] = 1
+            output_layer.append(temp)
+    return output_layer
+
+
+# get training data
+def get_training_data():
+    return get_input_from_samples(samples)
+
+
+# get test data
+def get_testing_data():
+    return get_input_from_samples(test)
